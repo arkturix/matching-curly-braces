0d756c8391b6934b48329aef0cc262d0313b7dbe
ADAM MAGNESON <adam.magneson@gmail.com>
2021-07-27 21:55:29 -0600

    Created main.js file with function

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// A function to test for matching braces in a string
+function matchingBraces(inputString) {
+    let n = 0;
+    for ( let i = 0; i < inputString.length; i++ ) {
+        if ( inputString[i] === '{' ) n++;
+        if ( inputString[i] === '}' ) {
+            n--;
+            if ( n < 0 ) return false;
+        }
+    }
+
+    return n === 0;
+}
